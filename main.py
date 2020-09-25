45feaf11bc11f2282d3bef3b7a68df9eefe9220d
Arsalan Ghassemi <contact@arsadevs.com>
2020-09-25 03:05:08 +0200

    Removed chromedriver from project and added it to path

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # Chrome Bot with Selenium
 from selenium import webdriver
+
+PATH = "/opt/WebDriver/bin/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://twitter.com")
+
+driver.find_element(By.LINK_TEXT, "Log in").click()
